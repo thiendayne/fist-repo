c09f422c69d68527e119678f0cf10a1142144ea8
thienluu <luuhoangthien28@gmail.com>
2024-09-04 19:37:12 +0700

    feat: updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fist-repo
+# test
---
+++ demo/src/main/java/com/example/demo/Repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.Repository;
+
+import com.example.demo.Entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
